b6a3617908034738c6c0cb8118ba5b12aa2d3406
Andrew Basner <andrewbasner@gmail.com>
2018-12-08 22:43:56 -0600

    change root route, fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
 
-  get 'pages/home', to: 'pages#home'
+  root 'pages#home'
+  # get 'pages/home', to: 'pages#home'
   get 'pages/about' => 'pages#about'
 
 end
