f6e7ee3b20991f6494d353767e87ec8e995c5171
Andrew Basner <andrewbasner@gmail.com>
2018-12-10 01:26:39 -0600

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password_confirmation, presence: true
 end
